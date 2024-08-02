c554e777a6c980e3ef94f8b0fc46e233d74c8f41
the_Ten_of_Hearts <ye_jin2004@163.com>
2024-08-02 14:25:12 +0800

    Adds readPlanet

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -10,21 +10,31 @@ class NBody {
         StdDraw.picture(0, 0, "./images/starfield.jpg");
     }
 
+    public static Planet[] readPlanets(String path) {
+        In in = new In(path);
+
+        int num = in.readInt();
+        in.readDouble();
+
+        Planet[] planets = new Planet[num];
+        for (int i = 0; i < num; i++) {
+            planets[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(),
+                    in.readString());
+        }
+
+        return planets;
+    }
+
     public static void main(String[] args) {
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
         String path = args[2];
 
         double radius = NBody.readRadius(path);
+        Planet[] planets = NBody.readPlanets(path);
+        int num = planets.length;
 
         In in = new In(path);
-        int num = in.readInt();
-        in.readDouble();
-        Planet[] planets = new Planet[num];
-        for (int i = 0; i < num; i++) {
-            planets[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(),
-                    in.readString());
-        }
 
         StdDraw.setScale(-radius, radius);
         StdDraw.enableDoubleBuffering();
@@ -54,13 +64,13 @@ class NBody {
             StdDraw.show();
             StdDraw.pause(10);
 
-            StdOut.printf("%d\n", planets.length);
-            StdOut.printf("%.2e\n", radius);
-            for (int i = 0; i < planets.length; i++) {
-                StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                        planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                        planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
-            }
+        }
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                    planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
         }
     }
 
