90fb5f2752d5aaaf1bf568bbbbc854f67213ad00
the_Ten_of_Hearts <ye_jin2004@163.com>
2024-08-03 19:33:40 +0800

    Finished IntList.reverse

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -1,73 +1,94 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class IntListTest {
+import static org.junit.Assert.*;
 
-    /**
-     * Example test that verifies correctness of the IntList.of static
-     * method. The main point of this is to convince you that
-     * assertEquals knows how to handle IntLists just fine.
-     */
+public class IntListTest
+{
 
-    @Test
-    public void testList() {
-        IntList one = new IntList(1, null);
-        IntList twoOne = new IntList(2, one);
-        IntList threeTwoOne = new IntList(3, twoOne);
+	/**
+	 * Example test that verifies correctness of the IntList.of static
+	 * method. The main point of this is to convince you that
+	 * assertEquals knows how to handle IntLists just fine.
+	 */
 
-        IntList x = IntList.of(3, 2, 1);
-        assertEquals(threeTwoOne, x);
-    }
+	@Test
+	public void testList()
+	{
+		IntList one = new IntList(1, null);
+		IntList twoOne = new IntList(2, one);
+		IntList threeTwoOne = new IntList(3, twoOne);
 
-    @Test
-    public void testdSquareList() {
-        IntList L = IntList.of(1, 2, 3);
-        IntList.dSquareList(L);
-        assertEquals(IntList.of(1, 4, 9), L);
-    }
+		IntList x = IntList.of(3, 2, 1);
+		assertEquals(threeTwoOne, x);
+	}
 
-    /**
-     * Do not use the new keyword in your tests. You can create
-     * lists using the handy IntList.of method.
-     * <p>
-     * Make sure to include test cases involving lists of various sizes
-     * on both sides of the operation. That includes the empty list, which
-     * can be instantiated, for example, with
-     * IntList empty = IntList.of().
-     * <p>
-     * Keep in mind that dcatenate(A, B) is NOT required to leave A untouched.
-     * Anything can happen to A.
-     */
+	@Test
+	public void testdSquareList()
+	{
+		IntList L = IntList.of(1, 2, 3);
+		IntList.dSquareList(L);
+		assertEquals(IntList.of(1, 4, 9), L);
+	}
 
-    @Test
-    public void testSquareListRecursive() {
-        IntList L = IntList.of(1, 2, 3);
-        IntList res = IntList.squareListRecursive(L);
-        assertEquals(IntList.of(1, 2, 3), L);
-        assertEquals(IntList.of(1, 4, 9), res);
-    }
+	/**
+	 * Do not use the new keyword in your tests. You can create
+	 * lists using the handy IntList.of method.
+	 * <p>
+	 * Make sure to include test cases involving lists of various sizes
+	 * on both sides of the operation. That includes the empty list, which
+	 * can be instantiated, for example, with
+	 * IntList empty = IntList.of().
+	 * <p>
+	 * Keep in mind that dcatenate(A, B) is NOT required to leave A untouched.
+	 * Anything can happen to A.
+	 */
 
-    @Test
-    public void testDcatenate() {
-        IntList A = IntList.of(1, 2, 3);
-        IntList B = IntList.of(4, 5, 6);
-        IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
-        assertEquals(exp, IntList.dcatenate(A, B));
-        assertEquals(IntList.of(1, 2, 3, 4, 5, 6), A);
-    }
+	@Test
+	public void testSquareListRecursive()
+	{
+		IntList L = IntList.of(1, 2, 3);
+		IntList res = IntList.squareListRecursive(L);
+		assertEquals(IntList.of(1, 2, 3), L);
+		assertEquals(IntList.of(1, 4, 9), res);
+	}
 
-    @Test
-    public void testCatenate() {
-        IntList A = IntList.of(1, 2, 3);
-        IntList B = IntList.of(4, 5, 6);
-        IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
-        assertEquals(exp, IntList.catenate(A, B));
-        assertEquals(IntList.of(1, 2, 3), A);
-    }
+	@Test
+	public void testDcatenate()
+	{
+		IntList A = IntList.of(1, 2, 3);
+		IntList B = IntList.of(4, 5, 6);
+		IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
+		assertEquals(exp, IntList.dcatenate(A, B));
+		assertEquals(IntList.of(1, 2, 3, 4, 5, 6), A);
+	}
 
-    /** If you're running this from the command line, you'll need
-      * to add a main method. See ArithmeticTest.java for an
-      * example. */
+	@Test
+	public void testCatenate()
+	{
+		IntList A = IntList.of(1, 2, 3);
+		IntList B = IntList.of(4, 5, 6);
+		IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
+		assertEquals(exp, IntList.catenate(A, B));
+		assertEquals(IntList.of(1, 2, 3), A);
+	}
+
+	/**
+	 * If you're running this from the command line, you'll need
+	 * to add a main method. See ArithmeticTest.java for an
+	 * example.
+	 */
+	@Test(timeout = 10000)
+	public void testReverse()
+	{
+		IntList A = IntList.of(1, 2, 3);
+		IntList exp = IntList.of(3, 2, 1);
+		assertEquals(exp, IntList.reverse(A));
+
+		assertNull(IntList.reverse(null));
+
+		IntList C = IntList.of(1, 2, 3);
+		IntList D = IntList.of(1, 2, 3);
+		assertNotEquals(D, IntList.reverse(C));
+	}
 
 }
