b9a3a746f276d7dfd548e51d5d3de3f85f373024
the_Ten_of_Hearts <ye_jin2004@163.com>
2024-08-05 13:24:26 +0800

    Finished the LinkedListDeque part of the project.

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,159 @@
+public class LinkedListDeque<T>
+{
+	private final Node<T> sentinel;
+	private int size;
+
+	/**
+	 * Constructor
+	 */
+	public LinkedListDeque()
+	{
+		size = 0;
+		sentinel = new Node<>(null, null, null);
+		sentinel.next = sentinel;
+		sentinel.prev = sentinel;
+	}
+
+	/**
+	 * Adds an element to the front of the list.
+	 */
+	public void addFirst(T item)
+	{
+		sentinel.next.prev = new Node<>(item, sentinel, sentinel.next);
+		sentinel.next = sentinel.next.prev;
+		size++;
+	}
+
+	/**
+	 * Adds an element to the last of the list.
+	 */
+	public void addLast(T item)
+	{
+		sentinel.prev.next = new Node<>(item, sentinel.prev, sentinel);
+		sentinel.prev = sentinel.prev.next;
+		size++;
+	}
+
+	/**
+	 * Checks if the list is empty.
+	 */
+	public boolean isEmpty()
+	{
+		return size() == 0;
+	}
+
+	/**
+	 * Returns the size of the list.
+	 */
+	public int size()
+	{
+		return size;
+	}
+
+	/**
+	 * Prints out the list.
+	 */
+	public void printDeque()
+	{
+		Node<T> p = sentinel.next;
+		while (p != sentinel)
+		{
+			System.out.print(p.data + " ");
+			p = p.next;
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Removes the first element in the list.
+	 */
+	public T removeFirst()
+	{
+		size--;
+		T data = sentinel.next.data;
+		sentinel.next = sentinel.next.next;
+		sentinel.next.prev = sentinel;
+		return data;
+	}
+
+	/**
+	 * Removes the last element in the list.
+	 */
+	public T removeLast()
+	{
+		size--;
+		T data = sentinel.prev.data;
+		sentinel.prev = sentinel.prev.prev;
+		sentinel.prev.next = sentinel;
+		return data;
+	}
+
+	/**
+	 * Gets the element at the index-th position iteratively.
+	 */
+	public T get(int index)
+	{
+		if (index < 0 || index >= size)
+		{
+			return null;
+		}
+		else
+		{
+			Node<T> p = sentinel.next;
+			while (index > 0)
+			{
+				p = p.next;
+				index--;
+			}
+			return p.data;
+		}
+	}
+
+	/**
+	 * Gets the index-th element in the list recursively.
+	 */
+	public T getRecursive(int index)
+	{
+		if (index < 0 || index >= size)
+		{
+			return null;
+		}
+		else
+		{
+			return sentinel.next.get(index);
+		}
+	}
+
+	/**
+	 * The Node that consists of the Deque
+	 */
+	private static class Node<T>
+	{
+		public T data;
+		public Node<T> next;
+		public Node<T> prev;
+
+		/**
+		 * The constructor of the Node class.
+		 */
+		public Node(T data, Node<T> prev, Node<T> next)
+		{
+			this.data = data;
+			this.next = next;
+			this.prev = prev;
+		}
+
+		/** The helper function that gets the index-th item in a naked DLList. */
+		public T get(int index)
+		{
+			if (index == 0)
+			{
+				return data;
+			}
+			else
+			{
+				return next.get(index - 1);
+			}
+		}
+	}
+}
--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,9 +1,12 @@
 /** Performs some basic linked list tests. */
-public class LinkedListDequeTest {
-	
+public class LinkedListDequeTest
+{
+
 	/* Utility method for printing out empty checks. */
-	public static boolean checkEmpty(boolean expected, boolean actual) {
-		if (expected != actual) {
+	public static boolean checkEmpty(boolean expected, boolean actual)
+	{
+		if (expected != actual)
+		{
 			System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
 			return false;
 		}
@@ -11,38 +14,45 @@ public class LinkedListDequeTest {
 	}
 
 	/* Utility method for printing out empty checks. */
-	public static boolean checkSize(int expected, int actual) {
-		if (expected != actual) {
+	public static boolean checkSize(int expected, int actual)
+	{
+		if (expected != actual)
+		{
 			System.out.println("size() returned " + actual + ", but expected: " + expected);
 			return false;
 		}
 		return true;
 	}
 
-	/* Prints a nice message based on whether a test passed. 
+	/* Prints a nice message based on whether a test passed.
 	 * The \n means newline. */
-	public static void printTestStatus(boolean passed) {
-		if (passed) {
+	public static void printTestStatus(boolean passed)
+	{
+		if (passed)
+		{
 			System.out.println("Test passed!\n");
-		} else {
+		}
+		else
+		{
 			System.out.println("Test failed!\n");
 		}
 	}
 
-	/** Adds a few things to the list, checking isEmpty() and size() are correct, 
-	  * finally printing the results. 
-	  *
-	  * && is the "and" operation. */
-	public static void addIsEmptySizeTest() {
+	/**
+	 * Adds a few things to the list, checking isEmpty() and size() are correct,
+	 * finally printing the results.
+	 * <p>
+	 * && is the "and" operation.
+	 */
+	public static void addIsEmptySizeTest()
+	{
 		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
 		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
 		lld1.addFirst("front");
-		
+
 		// The && operator is the same as "and" in Python.
 		// It's a binary operator that returns true if both arguments true, and false otherwise.
 		passed = checkSize(1, lld1.size()) && passed;
@@ -58,16 +68,14 @@ public class LinkedListDequeTest {
 		lld1.printDeque();
 
 		printTestStatus(passed);
-		*/
+
 	}
 
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
-	public static void addRemoveTest() {
+	public static void addRemoveTest()
+	{
 
 		System.out.println("Running add/remove test.");
-
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
@@ -81,10 +89,10 @@ public class LinkedListDequeTest {
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
 		printTestStatus(passed);
-		*/
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("Running tests.\n");
 		addIsEmptySizeTest();
 		addRemoveTest();
