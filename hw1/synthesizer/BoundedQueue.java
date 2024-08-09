946e2b608a0a2a1cd26466a1cad3c521e2c915ba
the_Ten_of_Hearts <ye_jin2004@163.com>
2024-08-09 21:17:16 +0800

    Finished Task 1 of hw1

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,45 @@
+package hw1.synthesizer;
+
+public interface BoundedQueue<T>
+{
+	/**
+	 * Returns the size of the buffer.
+	 */
+	int capacity();
+
+	/**
+	 * Returns number of items currently in the buffer.
+	 */
+	int fillCount();
+
+	/**
+	 * Adds item x to the end.
+	 */
+	void enqueue(T x);
+
+	/**
+	 * Deletes and returns the item from the front.
+	 */
+	T dequeue();
+
+	/**
+	 * Returns (but not delete) item from the front.
+	 */
+	T peek();
+
+	/**
+	 * Checks if the queue is empty
+	 */
+	default boolean isEmpty()
+	{
+		return fillCount() == 0;
+	}
+
+	/**
+	 * Checks if the queue is full.
+	 */
+	default boolean isFull()
+	{
+		return fillCount() == capacity();
+	}
+}
