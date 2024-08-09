2af27665f056b11f4435e1151719aabc5371a91f
the_Ten_of_Hearts <ye_jin2004@163.com>
2024-08-09 21:23:33 +0800

    Finished Task 2 of hw1

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,21 @@
+package hw1.synthesizer;
+
+import java.lang.annotation.Retention;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>
+{
+	protected int fillCount;
+	protected int capacity;
+
+	@Override
+	public int capacity()
+	{
+		return capacity;
+	}
+
+	@Override
+	public int fillCount()
+	{
+		return fillCount;
+	}
+}
