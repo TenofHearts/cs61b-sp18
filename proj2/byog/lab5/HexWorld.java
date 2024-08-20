5b68c0a3d1bb6e3da684e5776ecfa33695193f7f
the_Ten_of_Hearts <ye_jin2004@163.com>
2024-08-21 00:10:06 +0800

    Finished addHexagon in lab 5

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -1,5 +1,7 @@
 package byog.lab5;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import byog.TileEngine.TERenderer;
@@ -11,5 +13,39 @@ import java.util.Random;
 /**
  * Draws a world consisting of hexagonal regions.
  */
-public class HexWorld {
+public class HexWorld
+{
+	private static final int HEIGHT = 60;
+	private static final int WIDTH = 60;
+
+	public static void main(String[] args)
+	{
+		TERenderer renderer = new TERenderer();
+		renderer.initialize(WIDTH, HEIGHT);
+
+		TETile[][] world = new TETile[WIDTH][HEIGHT];
+		for (int i = 0; i < WIDTH; i++)
+		{
+			for (int j = 0; j < HEIGHT; j++)
+			{
+				world[i][j] = Tileset.NOTHING;
+			}
+		}
+
+		addHexagon(10, 10, 10, Tileset.FLOWER, world);
+
+		renderer.renderFrame(world);
+	}
+
+	public static void addHexagon(int size, int leftUpperCornerX, int leftUpperCornerY, TETile tile, TETile[][] world)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = leftUpperCornerX - i; j < leftUpperCornerX + size + i; j++)
+			{
+				world[j][leftUpperCornerY + i] = tile;
+				world[j][leftUpperCornerY + 2 * size - 1 - i] = tile;
+			}
+		}
+	}
 }
