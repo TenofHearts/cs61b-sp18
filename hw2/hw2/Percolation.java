ea0576519524775fb960a107112a9d3ef1612812
the_Ten_of_Hearts <ye_jin2004@163.com>
2024-09-02 10:15:53 +0800

    Finished the basic structure of Percolation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,9 +1,113 @@
-package hw2.hw2;
+package hw2;
 
+import com.sun.source.doctree.EscapeTree;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+import java.awt.image.renderable.RenderableImage;
 
 public class Percolation
 {
-    public Percolation(int n)
-    {}
+	private final int LEN;
+	private WeightedQuickUnionUF ds;
+	private boolean[][] graph;
+
+	public Percolation(int n)
+	{
+		LEN = n;
+
+		ds = new WeightedQuickUnionUF(n * n);
+
+		graph = new boolean[n][n];
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				graph[i][j] = false;
+			}
+		}
+	}
+
+	private void connectUp(int i)
+	{
+		if (i < LEN)
+		{
+			return;
+		}
+		else
+		{
+			ds.union(i, i - LEN);
+		}
+	}
+
+	private void connectDown(int i)
+	{
+		if (i >= LEN * (LEN - 1))
+		{
+			return;
+		}
+		else
+		{
+			ds.union(i, i + LEN);
+		}
+	}
+
+	private void connectLeft(int i)
+	{
+		if (i % LEN == 0)
+		{
+			return;
+		}
+		else
+		{
+			ds.union(i, i - 1);
+		}
+	}
+
+	private void connectRight(int i)
+	{
+		if ((i + 1) % LEN == 0)
+		{
+			return;
+		}
+		else
+		{
+			ds.union(i, i + 1);
+		}
+	}
+
+	private void connectAll(int row, int col)
+	{
+		int index = row * LEN + col;
+
+		connectDown(index);
+		connectLeft(index);
+		connectUp(index);
+		connectRight(index);
+	}
+
+	public void open(int row, int col)
+	{
+		graph[row][col] = true;
+		connectAll(row, col);
+	}
+
+	public boolean isOpen(int row, int col)
+	{
+		return graph[row][col];
+	}
+
+	public boolean isFull(int row, int col)
+	{
+		return !isOpen(row, col);
+	}
+
+	public int numberOfOpenSites()
+	{
+		return 0;
+	}
+
+	public boolean percolates()
+	{
+		return false;
+	}
 }
