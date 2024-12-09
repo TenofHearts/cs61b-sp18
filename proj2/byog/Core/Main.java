284bcbd9b85ed25e12ca10145d200d5b644f25d5
the_Ten_of_Hearts <ye_jin2004@163.com>
2024-12-09 14:22:12 +0800

    refresh

--- proj2/byog/Core/Main.java
+++ proj2/byog/Core/Main.java
@@ -2,6 +2,8 @@ package byog.Core;
 
 import byog.TileEngine.TETile;
 
+import java.util.Scanner;
+
 /**
  * This is the main entry point for the program. This class simply parses
  * the command line inputs, and lets the byog.Core.Game class take over
@@ -11,21 +13,34 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		if (args.length > 1)
+		// if (args.length > 1)
+		//	{
+		//		System.out.println("Can only have one argument - the input string");
+		//		System.exit(0);
+		//	}
+		//	else if (args.length == 1)
+		//	{
+		//		Game game = new Game();
+		//		TETile[][] worldState = game.playWithInputString(args[0]);
+		//		System.out.println(TETile.toString(worldState));
+		//	}
+		//	else
+		//	{
+		//		Game game = new Game();
+		//		game.playWithKeyboard();
+		//	}
+		Scanner scanner = new Scanner(System.in);
+		String arg = scanner.nextLine();
+
+		Game game = new Game();
+		if (arg.equals("start"))
 		{
-			System.out.println("Can only have one argument - the input string");
-			System.exit(0);
-		}
-		else if (args.length == 1)
-		{
-			Game game = new Game();
-			TETile[][] worldState = game.playWithInputString(args[0]);
-			System.out.println(TETile.toString(worldState));
+			game.playWithKeyboard();
 		}
 		else
 		{
-			Game game = new Game();
-			game.playWithKeyboard();
+			TETile[][] worldState = game.playWithInputString(arg);
+			System.out.println(TETile.toString(worldState));
 		}
 	}
 }
